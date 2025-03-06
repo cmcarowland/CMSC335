f0b6c2406a0c400fdfad62fd66d0f9f41d991976
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-03-06 10:54:27 +0000

    Refactored Menu System
    feat: Created base Menu class for menus to derive from
    feat: Created MainMenu, TwoDShapeMenu, and ThreeDShapeMenu classes
    refactor: Main method to use the new menu system

---
+++ Project1/src/TwoDShapeMenu.java
@@ -0,0 +1,49 @@
+public class TwoDShapeMenu extends Menu {     
+
+    public TwoDShapeMenu() {
+        super();
+        menuName = "2D Shape Menu";
+    }
+
+    public TwoDShapeMenu(Menu parent) {
+        parentMenu = parent;
+        menuName = "2D Shape Menu";
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n" + menuName + "\n");
+        sb.append("---------------------\n");
+        sb.append("1) Construct a Circle\n");
+        sb.append("2) Construct a Rectangle\n");
+        sb.append("3) Construct a Square\n");
+        sb.append("4) Construct a Triangle\n");
+        sb.append("0) Return to Main\n");
+        return sb.toString();
+    }
+
+    public boolean validateUserInput(int userInput) {
+        return userInput >= 0 && userInput <= 4;
+    }
+
+    @Override
+    public Menu run()  {
+        System.out.println(toString());
+        var userInput = parseUserInt();
+        if(!validateUserInput(userInput)) {
+            System.err.println("Invalid input. Please select an appropriate option.");
+        }
+
+        switch(userInput) {
+            case 1:
+                return this;
+            case 2:
+                return this;
+            case 0:
+                return parentMenu;
+            default:
+                return this;
+        }
+    }
+}
