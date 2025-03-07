c9c2422d7ac6db82554696a99ce7d14883e87395
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-03-07 10:37:20 +0000

    Implemented Shape Object
    feat: Created Shape Object
    feat: Implemented Shape calculate volume function
    feat: Over rode toString method to output shape details
    
    Closes: #4

---
+++ Project1/src/Sphere.java
@@ -0,0 +1,30 @@
+public class Sphere extends ThreeDimensionalShape {
+    private float radius;
+
+    public Sphere() {
+        super();
+        radius = 0;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public float calculateVolume() {
+        return (float) (4.0 / 3.0 * Math.PI * Math.pow(radius, 3));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(shapeData);
+        sb.append("    -Radius : %.2f\n");
+        sb.append("    -Volume : %.2f\n");
+        return String.format(sb.toString(), this.getClass().getSimpleName(), radius, calculateVolume());
+    }
+    
+}
