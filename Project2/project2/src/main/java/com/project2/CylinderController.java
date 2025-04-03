064164ef70b2b562cd521f02991eaa733acfab75
Quatro Designs <cmcarowland@live.com>
2025-04-03 16:17:41 -0400

    refactor(controller): Refactored Cylinder code and fxml to inherit from base

--- Project2/project2/src/main/java/com/project2/CylinderController.java
+++ Project2/project2/src/main/java/com/project2/CylinderController.java
@@ -1,18 +1,12 @@
 package com.project2;
 
-import java.io.IOException;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
 
 import com.shapes.Cylinder;
 
-public class CylinderController {
+public class CylinderController extends BaseController {
     @FXML
     private ComboBox<String> box1;
     @FXML
@@ -46,23 +40,5 @@ public class CylinderController {
 
     static public String getDimensions() {
         return String.valueOf((int)cylinder.getRadius()) + "X" + String.valueOf((int)cylinder.getHeight());
-    }
-
-    @FXML
-    void cancelClicked(ActionEvent ae) {
-        // Load an additional FXML file
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("baseSettings.fxml"));
-        Node additionalContent;
-        try {
-            additionalContent = loader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        // Add the additional content to the main layout
-        GridPane mainLayout = (GridPane) ((Node) ae.getSource()).getScene().getRoot();
-        mainLayout.getChildren().add(additionalContent);
-        App.shapeType = "";
     }
 }
