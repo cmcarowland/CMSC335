4f5f224a9d026267bea9abce52d85bfa26764496
Quatro Designs <cmcarowland@live.com>
2025-04-03 16:09:26 -0400

    refactor: Cone to inherit from base layout and controller

--- Project2/project2/src/main/java/com/project2/ConeController.java
+++ Project2/project2/src/main/java/com/project2/ConeController.java
@@ -1,18 +1,12 @@
 package com.project2;
 
-import java.io.IOException;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
 
 import com.shapes.Cone;
 
-public class ConeController {
+public class ConeController extends BaseController {
     @FXML
     private ComboBox<String> box1;
     @FXML
@@ -48,23 +42,5 @@ public class ConeController {
 
     static public String getDimensions() {
         return String.valueOf((int)cone.getRadius()) + "X" + String.valueOf((int)cone.getHeight());
-    }
-
-    @FXML
-    void cancelClicked(ActionEvent ae) {
-        // Load an additional FXML file
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("baseSettings.fxml"));
-        Node additionalContent;
-        try {
-            additionalContent = loader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        // Add the additional content to the main layout
-        GridPane mainLayout = (GridPane) ((Node) ae.getSource()).getScene().getRoot();
-        mainLayout.getChildren().add(additionalContent);
-        App.shapeType = "";
     }
 }
