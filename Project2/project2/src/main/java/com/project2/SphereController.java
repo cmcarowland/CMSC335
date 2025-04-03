1a03ce4ba692cf5ed3bd2cc7b9ab329a86fdf226
Quatro Designs <cmcarowland@live.com>
2025-04-03 16:22:11 -0400

    refactor(controller): Refactored sphere code and fxml to inherit from base

--- Project2/project2/src/main/java/com/project2/SphereController.java
+++ Project2/project2/src/main/java/com/project2/SphereController.java
@@ -1,18 +1,12 @@
 package com.project2;
 
-import java.io.IOException;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
 
 import com.shapes.Sphere;
 
-public class SphereController {
+public class SphereController extends BaseController {
     @FXML
     private ComboBox<String> box1;
     @FXML
@@ -39,23 +33,5 @@ public class SphereController {
 
     static public double getRadius() {
         return sphere.getRadius();
-    }
-
-    @FXML
-    void cancelClicked(ActionEvent ae) {
-        // Load an additional FXML file
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("baseSettings.fxml"));
-        Node additionalContent;
-        try {
-            additionalContent = loader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        // Add the additional content to the main layout
-        GridPane mainLayout = (GridPane) ((Node) ae.getSource()).getScene().getRoot();
-        mainLayout.getChildren().add(additionalContent);
-        App.shapeType = "";
     }
 }
