5789ccbeaae8fd36a5a615aba8a4ed2305a9c28c
Quatro Designs <cmcarowland@live.com>
2025-04-17 22:07:46 -0400

    refactor: Added various buttons, extracted clear cars to a  method, implemented button logic for simulation control and blocked addition of resources if sim is not running

--- project3/src/main/java/com/project3/PrimaryController.java
+++ project3/src/main/java/com/project3/PrimaryController.java
@@ -3,6 +3,8 @@ package com.project3;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import java.util.List;
@@ -14,6 +16,14 @@ public class PrimaryController implements TimeTickedListener {
     private VBox leftLane;
     @FXML
     private VBox rightLane;
+    @FXML
+    private Button stopBtn;
+    @FXML
+    private Button playBtn;
+    @FXML
+    private Button contBtn;
+    @FXML
+    private Button pauseBtn;
 
     @Override
     public void onTimeTicked(String time) {
@@ -26,9 +36,13 @@ public class PrimaryController implements TimeTickedListener {
         timeLabel.setText(time);
     }
 
-    public void processCars(List<PhysicsTickListener> cars) {
+    public void clearCars() {
         leftLane.getChildren().clear();
         rightLane.getChildren().clear();
+    }
+
+    public void processCars(List<PhysicsTickListener> cars) {
+        clearCars();
         for (PhysicsTickListener ptl : cars) {
             if (!(ptl instanceof Car)) {
                 continue;
@@ -44,7 +58,58 @@ public class PrimaryController implements TimeTickedListener {
     }
 
     @FXML
+    private void stop(ActionEvent ae) {
+        contBtn.setDisable(true);
+        stopBtn.setDisable(true);
+        pauseBtn.setDisable(true);
+        playBtn.setDisable(false);
+        App.setCurrentState(SimulationState.STOPPED);
+        Physics.reset();
+        App.resetScene();
+    }
+
+    @FXML
+    private void play(ActionEvent ae) {
+        playBtn.setDisable(true);
+        stopBtn.setDisable(false);
+        contBtn.setDisable(true);
+        pauseBtn.setDisable(false);
+        App.setCurrentState(SimulationState.RUNNING);
+    }
+
+    @FXML
+    private void pause(ActionEvent ae) {
+        playBtn.setDisable(true);
+        stopBtn.setDisable(false);
+        contBtn.setDisable(false);
+        pauseBtn.setDisable(true);
+        App.setCurrentState(SimulationState.PAUSED);
+    }
+
+    @FXML
+    private void continueClicked(ActionEvent ae) {
+        playBtn.setDisable(true);
+        stopBtn.setDisable(false);
+        contBtn.setDisable(true);
+        pauseBtn.setDisable(false);
+        App.setCurrentState(SimulationState.RUNNING);
+    }
+
+    private void showDialog(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    @FXML
     private void addCarBottom(ActionEvent ae) {
+        if(App.currentState != SimulationState.RUNNING) {
+            showDialog("Error", "Simulation is not running, press play to start it and then add cars.");
+            return;
+        }
+        
         Car car = new Car(true);
         App.mainLayout.add(car.getCircle(), 0, 2 + (car.isToRight() ? 2 : 0));
         Physics.getCarsGoingX(car);
@@ -53,6 +118,11 @@ public class PrimaryController implements TimeTickedListener {
     
     @FXML
     private void addCarTop(ActionEvent ae) {
+        if(App.currentState != SimulationState.RUNNING) {
+            showDialog("Error", "Simulation is not running, press play to start it and then add cars.");
+            return;
+        }
+
         Car car = new Car(false);
         App.mainLayout.add(car.getCircle(), 0, 2 + (car.isToRight() ? 2 : 0));
         Physics.getCarsGoingX(car);
@@ -61,6 +131,11 @@ public class PrimaryController implements TimeTickedListener {
 
     @FXML
     private void addIntersection(ActionEvent ae) {
+        if(App.currentState != SimulationState.RUNNING) {
+            showDialog("Error", "Simulation is not running, press play to start it and then add intersections.");
+            return;
+        }
+
         for (Intersection i : App.intersectionList) {
             if(i.getState() == LightState.OFF) {
                 i.enable();
